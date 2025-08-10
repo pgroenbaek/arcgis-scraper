922ec133a65b9d4c3c449734da758230c6880180
Peter Grønbæk Andersen <peter@grnbk.io>
2025-08-10 09:05:16 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="arcgis-scraper",
+    version="0.0.1",
+    description="Placeholder package to reserve the name",
+    author="Peter Grønbæk Andersen",
+    packages=[],
+)
